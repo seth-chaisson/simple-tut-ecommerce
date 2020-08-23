3f70be8b18273bf82071de40ce36bc06f3bcc3ed
seth <seth.chaisson@gmail.com>
2020-08-23 17:54:22 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "./components/header";
+import "./default.scss";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h2>Hello World!</h2>
     </div>
   );
